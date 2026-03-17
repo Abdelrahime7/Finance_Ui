fcc234bf05f9435b6b53419a4063cb78627dc9d1
Abdelrahime7 <abdououni67@gmail.com>
2026-03-17 15:11:02 +0100

    feat: integrate screenutil and styling updates - modified colors.dart for extended palette - added style.dart with AppTextStyle definitions - wrapped MaterialApp with ScreenUtilInit - refreshed pubspec.lock and pubspec.yaml for screenutil dependency

--- finance_ui/lib/const/styling/colors.dart
+++ finance_ui/lib/const/styling/colors.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
-class Colors {
+class AppColors {
   static Color primaryColor=const Color(0xff617AFD);
   static Color secondaryColr= const Color(0xff8391A1);
+  static Color black=const Color(0xff1F2C37);
+  static Color grey=const Color(0xff9CA4AB);
 }
---
+++ finance_ui/lib/const/styling/style.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+import 'package:finance_ui/const/styling/colors.dart';
+import 'package:finance_ui/const/styling/fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Style {
+  static TextStyle titleStyle = TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.primaryColor,
+    fontSize: 30.sp,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle subtitleStyle = TextStyle(
+     fontFamily: Fonts.FontName,
+    color: AppColors.secondaryColr,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle bodyStyle = TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.black,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  );
+   static TextStyle greybodyStyle = TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.grey,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+}
