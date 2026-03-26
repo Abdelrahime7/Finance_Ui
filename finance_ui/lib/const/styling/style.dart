1974c2d02945db84b25714337c095e1bf7942722
Abdelrahime7 <abdououni67@gmail.com>
2026-03-26 09:57:52 +0100

    customize primary elevated and outlined buttons for reusabillity across app

--- finance_ui/lib/const/styling/style.dart
+++ finance_ui/lib/const/styling/style.dart
@@ -4,27 +4,27 @@ import 'package:finance_ui/const/styling/fonts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Style {
-  static TextStyle  titleStyle  = TextStyle(
+  static TextStyle get titleStyle => TextStyle(
     fontFamily: Fonts.FontName,
     color: AppColors.primaryColor,
     fontSize: 30.sp,
     fontWeight: FontWeight.bold,
   );
 
-  static TextStyle subtitleStyle = TextStyle(
+  static   TextStyle  get  subtitleStyle => TextStyle(
      fontFamily: Fonts.FontName,
     color: AppColors.secondaryColr,
     fontSize: 16.sp,
     fontWeight: FontWeight.w400,
   );
 
-  static TextStyle bodyStyle = TextStyle(
+  static   TextStyle  get bodyStyle =>TextStyle(
     fontFamily: Fonts.FontName,
     color: AppColors.black,
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
-   static TextStyle greybodyStyle = TextStyle(
+   static     TextStyle get   greybodyStyle => TextStyle(
     fontFamily: Fonts.FontName,
     color: AppColors.grey,
     fontSize: 12.sp,
--- finance_ui/lib/const/styling/theme.dart
+++ finance_ui/lib/const/styling/theme.dart
@@ -1,18 +1,41 @@
 import 'package:finance_ui/const/styling/colors.dart';
-import 'package:finance_ui/const/styling/style.dart';
+import 'package:finance_ui/const/styling/fonts.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AppThemes{
 
- static  ThemeData lightTheme = ThemeData(
-  primaryColor: AppColors.primaryColor,
+ static final ThemeData lightTheme = ThemeData(
+  primaryColor: AppColors.black,
 
-scaffoldBackgroundColor: AppColors.white,
- textTheme: TextTheme(
-  displayLarge: Style.titleStyle as TextStyle?, 
-  titleMedium: Style.subtitleStyle as TextStyle?,
-  bodyLarge: Style.bodyStyle as TextStyle?,
-  bodySmall: Style.greybodyStyle as TextStyle?,
+scaffoldBackgroundColor: AppColors.black,
+ textTheme: TextTheme (
+  displayLarge: TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.primaryColor,
+    fontSize: 30.sp,
+    fontWeight: FontWeight.bold,
+  ),
+
+  titleMedium: TextStyle(
+     fontFamily: Fonts.FontName,
+    color: AppColors.secondaryColr,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  )
+  ,
+  bodyLarge:  TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.black,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  ),
+  
+  bodySmall:  TextStyle(
+    fontFamily: Fonts.FontName,
+    color: AppColors.grey,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  ) ,
 ),
 buttonTheme: ButtonThemeData(
   buttonColor: AppColors.primaryColor,
